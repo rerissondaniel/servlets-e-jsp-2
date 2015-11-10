d8c9e8f6b45755ff63d2064dcf7440b8a55cec12
Rerisson Daniel <rerissondaniel@gmail.com>
2015-11-10 19:35:02 -0300

    tried to fix filter

--- src/util/SecurityFilter.java
+++ src/util/SecurityFilter.java
@@ -5,14 +5,12 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import core.Administrator;
 
 public class SecurityFilter implements Filter{
 
@@ -26,11 +24,12 @@ public class SecurityFilter implements Filter{
 			throws IOException, ServletException {
 		
 		HttpSession session = ((HttpServletRequest) request).getSession();
-		Administrator admin = (Administrator) session.getAttribute("username");
+		String admin = (String) session.getAttribute("username");
 		
 		if(admin == null){
-			request.setAttribute("mensagem", "Você não está autorizado a vizualizar esta página");
-			((HttpServletResponse)response).sendRedirect("webapp/index.jsp");
+			request.setAttribute("error", "Você não está autorizado a vizualizar esta página");
+			RequestDispatcher dispatcher = request.getRequestDispatcher("webapp/index.jsp");
+			dispatcher.forward(request, response);
 			
 		}else{
 			chain.doFilter(request, response);
